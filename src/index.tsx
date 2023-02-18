19411328de288ed51be6c02f40750628e05c12ee
kocierik <kocierik@gmail.com>
2023-02-18 12:51:32 +0100

    feat: setup tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-
+import '../tailwind.css'
 // Delete me
 export const Thing = () => {
-  return <div>initialize.</div>
+  return <div className='text-blue-400 mt-10'>initialize.</div>
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
