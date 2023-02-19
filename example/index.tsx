b05480126e2879118f1e4898d0a7dadefd679349
kocierik <kocierik@gmail.com>
2023-02-20 00:48:35 +0100

    feat: add example test

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,29 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../';
+import { DropDown, Setting } from '@kocierik/dropdown-menu';
+
+interface Isetting {
+  label: string;
+  handler: (...args: string[]) => void;
+}
 
 const App = () => {
+  const setting: Isetting[] = [{
+    label: 'helper',
+    handler: () => {
+      alert('help!')
+    }
+  }, {
+    label: 'Click me',
+    handler: () => {
+      alert('clicked!')
+    }
+  }
+  ]
   return (
-    <div>
-      <Thing />
+    <div className='flex flex-1 p-5 justify-center'>
+      <Setting settings={setting} />
     </div>
   );
 };
