accb1a78ca0c30b042eac0e37dcb025a99975691
kocierik <kocierik@gmail.com>
2023-02-19 21:38:19 +0100

    fix: export

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,5 @@
-// import * as React from 'react'
-// import DropDown from './components/dropDown/DropDown'
-// import Setting from './components/filter/Setting'
-
-// export const Thing = () => {
-
-//   return <div className='text-blue-400 mt-10 flex flex-1 items-center justify-center'>
-//     <DropDown label={'label'} list={["label"]} filter={[]} setFilter={function (_value: React.SetStateAction<string[]>): void {
-//       throw new Error('Function not implemented.')
-//     }} />
-//     <Setting settingInfoDesk={[{ label: "ciaoa", handler: () => { } }]} />
-//   </div>
-// }
+import { DropDown } from './components/dropDown/DropDown'
+import { Setting } from './components/filter/Setting'
 
 export * from './components/dropDown/DropDown'
 export * from './components/filter/Setting'
