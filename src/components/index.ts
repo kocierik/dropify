1a7d1f9af25c946d38c5d9e3b61917adf5811db5
kocierik <kocierik@gmail.com>
2023-02-19 22:01:38 +0100

    feat: add export

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { DropDown } from "./dropDown";
+export { Setting } from "./filter";
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { DropDown } from './components/dropDown/DropDown'
-import { Setting } from './components/filter/Setting'
-
-export * from './components/dropDown/DropDown'
-export * from './components/filter/Setting'
