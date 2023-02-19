5a5d32bd60aba653d27410eab56d43fea122ea50
kocierik <kocierik@gmail.com>
2023-02-19 21:15:34 +0100

    chore: add basic test

---
+++ test/dropDown.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import DropDown from '../src/components/dropDown/DropDown'
+
+describe('it', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<DropDown label={'label'} list={["label"]} filter={[]} setFilter={function (_value: React.SetStateAction<string[]>): void {
+      throw new Error('Function not implemented.')
+    }} />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
